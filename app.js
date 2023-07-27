acc9b0382526eb1cb0f4b5c22d1520ffc2b84e3f
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-27 06:50:44 -0300

    liberando cors domain

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors'); // Importe o pacote 'cors'
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
