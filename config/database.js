fcb3013c4f431dced7b793df9dcbf4afcac75bab
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-27 06:59:45 -0300

    configurando variável de ambiente

--- config/database.js
+++ config/database.js
@@ -1,9 +1,16 @@
 // database.js
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('fluxo_de_caixa_nodejs', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql', // Ou substitua por outro dialeto do banco de dados suportado pelo Sequelize
-});
+
+
+const sequelize = new Sequelize(
+  (process.env.DATABASE || 'fluxo_de_caixa_nodejs'),
+  (process.env.DATABASE_USER || 'root'),
+  (process.env.DATABASE_PASS || 'root'),
+  {
+    host: (process.env.DATABASE_HOST || 'localhost'),
+    dialect: 'mysql', // Ou substitua por outro dialeto do banco de dados suportado pelo Sequelize
+  }
+);
 
 module.exports = sequelize;
