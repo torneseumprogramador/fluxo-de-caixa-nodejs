6709c1acbf7fd476494722f43831ed9581483341
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-27 06:49:01 -0300

    api

---
+++ routes/api.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const Caixa = require('../models/caixa');
+
+/* GET home page. */
+router.get('/caixas', async function(req, res, next) {
+  try {
+    // Aqui você pode utilizar o modelo "Caixa" para obter os dados do banco de dados
+    const extrato = await Caixa.findAll(); // Supondo que você tem um método findAll() definido no modelo Caixa
+
+    // Inicializa as variáveis de receitas e despesas
+    let receitas = 0;
+    let despesas = 0;
+
+    // Itera pelos lançamentos e calcula as receitas e despesas
+    for (const lancamento of extrato) {
+      if (lancamento.status === 1) {
+        receitas += lancamento.valor;
+      } else if (lancamento.status === 0) {
+        despesas += lancamento.valor;
+      }
+    }
+
+    // Calcula o valor total
+    const valor_total = receitas - despesas;
+
+    res.send({ 
+      valor_total: valor_total,
+      receitas: receitas,
+      despesas: despesas,
+      extrato: extrato
+    });
+
+  } catch (error) {
+    res.send(error).status(500);
+  }
+});
+
+router.delete('/caixas/:id', async function(req, res, next) {
+  try {
+    const id = req.params.id;
+    // Encontre o lançamento a ser excluído pelo ID
+    const lancamento = await Caixa.findByPk(id);
+
+    if (!lancamento) {
+      // Se o lançamento não for encontrado, redirecionar para a página inicial
+      return res.send({ mensagem: "Não existe este registro" }).status(404);
+    }
+
+    // Excluir o lançamento do banco de dados
+    await lancamento.destroy();
+
+    return res.send({}).status(204);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/caixas', async function(req, res, next) {
+  try {
+    // Obter os dados do formulário
+    const { tipo, valor, status } = req.body;
+
+    // Inserir os dados no banco de dados usando o modelo "Caixa"
+    const caixa = await Caixa.create({ tipo, valor, status });
+
+    res.send(caixa).status(201);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
